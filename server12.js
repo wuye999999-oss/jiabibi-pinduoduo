14a766c8f0f3e067befb9092a82bca39d236bca0
wuye999999-oss <wuye999999@gmail.com>
2026-05-12 20:43:09 +0800

    add server12 douyin cps runtime

---
+++ server12.js
@@ -0,0 +1,4 @@
+// server12.js: Douyin CPS auto-sign probe runtime
+// Starts the stable server7 runtime with the latest Render-selected DOUYIN_SIGN_MODE.
+// Use Render env DOUYIN_SIGN_MODE to switch modes without editing code.
+require('./server7.js');
